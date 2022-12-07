d006edc42067d5953e09436adf760a374d0159fa
fmael <mael.fath@ynov.com>
2022-12-07 13:56:33 +0100

    Changes by alexandre

--- src/src/eu/bonfire/inter/Inter.java
+++ src/src/eu/bonfire/inter/Inter.java
@@ -4,9 +4,9 @@ import eu.bonfire.Main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Inter {
-
     private Main main;
 
     public Inter(Main main) {
@@ -14,16 +14,22 @@ public class Inter {
     }
 
     public void launchInter() {
-        JTextArea jta = new JTextArea(20, 50);
-        JFrame f = new JFrame("Bonfire");
-        String text = "Welcome to our chatbox";
-        JTextArea textArea = new JTextArea(text, 5, 10);
-        textArea.setPreferredSize(new Dimension(100, 100));
-        f.setVisible(true);
-        f.setSize(1920, 1080);
-        JScrollPane jsp = new JScrollPane(jta);
-        f.add(textArea);
-        f.add(jsp, BorderLayout.CENTER);
-        f.pack();
+        JPanel textPanel= new JPanel();
+        JFrame bonfire= new JFrame();
+        JLabel textLabel= new JLabel();
+        bonfire.setSize(800, 600);
+        bonfire.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bonfire.getContentPane().setBackground(Color.black);
+        bonfire.setLayout(null);
+
+        bonfire.setVisible(true);
+
+        textPanel = new JPanel();
+        textPanel.setBounds(150, 250, 500, 100);
+        textPanel.setBackground(Color.blue);
+        textLabel = new JLabel();
+        textLabel.setBackground(Color.white);
+        textPanel.add(textLabel);
+
     }
 }
